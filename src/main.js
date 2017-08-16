a631de21d828ffcb4bfaa4ad5f68c089cb998029
try46 <popuman61@gmail.com>
2017-08-16 16:12:15 +0900

    全てを実装した 各種インデントを整えた

--- src/main.js
+++ src/main.js
@@ -1,45 +1,41 @@
 (function() {
-			'use strict';
-			var slider = document.getElementById('slider');
-			var label = document.getElementById('label');
-			var btn = document.getElementById('btn');
-			var result = document.getElementById('result');
-			var number=document.getElementById('number');
-			var symbols=document.getElementById('symbols');
-			function getPassword() {
-				var seed_letters = 'abcdefghijklmnopqrstuvwxyz';
-				var seed_numbers='0123456789';
-				var seed_symbols='!""#$%&()"=~^|_/?.>,<';
-				var seed;
-				var len = slider.value;
-				var pwd = '';
-				seed=seed_letters+seed_letters.toUpperCase();
-				if(numbers.checked===true){
-					seed+=seed_numbers;
-				}
-				if(symbols.checked){
-					seed+=seed_symbols;
-				}
-				/**
-				for (var i = 0; i < len; i++) {
-					pwd += seed[Math.floor(Math.random() * seed.length)];
-				}
-				result.value = pwd;
-				*/
-				while (len--) {
-					pwd += seed[Math.floor(Math.random() * seed.length)];
-				}
-				result.value = pwd;
-			}
-			slider.addEventListener('change', function() {
-				label.innerHTML = this.value;
-			});
-			btn.addEventListener('click', function() {
-				getPassword();
-				//result.value = 'skldjfkljasjlfdjsahdofj';
-			});
-			result.addEventListener('click', function() {
-				this.select();
-			});
-			getPassword();
-		})();
+  'use strict';
+  var slider = document.getElementById('slider');
+  var label = document.getElementById('label');
+  var btn = document.getElementById('btn');
+  var result = document.getElementById('result');
+  var number = document.getElementById('number');
+  var symbols = document.getElementById('symbols');
+
+  function getPassword() {
+    var seed_letters = 'abcdefghijklmnopqrstuvwxyz';
+    var seed_numbers = '0123456789';
+    var seed_symbols = '!""#$%&()"=~^|_/?.>,<';
+    var seed;
+    var len = slider.value;
+    var pwd = '';
+    seed = seed_letters + seed_letters.toUpperCase();
+    if (numbers.checked === true) {
+      seed += seed_numbers;
+    }
+    if (symbols.checked) {
+      seed += seed_symbols;
+    }
+
+    while (len--) {
+      pwd += seed[Math.floor(Math.random() * seed.length)];
+    }
+    result.value = pwd;
+  }
+  slider.addEventListener('change', function() {
+    label.innerHTML = this.value;
+  });
+  btn.addEventListener('click', function() {
+    getPassword();
+    //result.value = 'skldjfkljasjlfdjsahdofj';
+  });
+  result.addEventListener('click', function() {
+    this.select();
+  });
+  getPassword();
+})();
